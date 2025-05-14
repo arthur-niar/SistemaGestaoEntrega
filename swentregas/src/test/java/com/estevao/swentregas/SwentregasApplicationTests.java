519048dde154d0c3158c6272e8cd241345a0b82e
Renezoka <rmmedeiros300@gmail.com>
2025-05-14 18:44:38 -0300

    aiaiai

---
+++ swentregas/src/test/java/com/estevao/swentregas/SwentregasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estevao.swentregas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwentregasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
