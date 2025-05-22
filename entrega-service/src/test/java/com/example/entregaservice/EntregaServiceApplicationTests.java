bbbd0afc5d6465ef7a8e953186768ca895b8a542
soulzuhn <rhyandarocha2006@gmail.com>
2025-05-22 14:49:27 -0300

    Adicionando Arquitetura Microserviços

---
+++ entrega-service/src/test/java/com/example/entregaservice/EntregaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.entregaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
